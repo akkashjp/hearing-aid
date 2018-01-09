4c45560bc1b9e38e7c562d5100226418b9ddcf26
prakashdilli <prakashece245@gmail.com>
2018-01-09 11:07:01 +0530

    start of 9/01

--- src/app/eodtally/eodtally.component.ts
+++ src/app/eodtally/eodtally.component.ts
@@ -8,23 +8,50 @@ import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class EodtallyComponent implements OnInit {
   eodTally: FormGroup;
+  thousands :number;
+  fivehundreds:number;
+  hundreds:number;
+  fifty:number;
+  tens:number;
+  fives:number;
+  ones:number;
   constructor(private fb: FormBuilder) { }
+
+  total:number;
 
   ngOnInit() {
     this.eodTally = this.fb.group({
-      Thousands: [''],
-      FiveHundreds:[''],
-      Hundreds:  [''],
-      Fiftys:[''],
-      Tens: [''],
-      Fives:[''],
-      Ones:[''],
-      CashDifference:[''],        
+      Thousands: [0],
+      FiveHundreds:[0],
+      Hundreds:  [0],
+      Fiftys:[0],
+      Tens: [0],
+      Fives:[0],
+      Ones:[0],
+      CashDifference:[0],        
     });
+
+    
   }
 
   onSubmit(){
     console.log('eodTally',this.eodTally.value);
+  }
+
+  onKeyup(){
+    this.thousands = this.eodTally.get('Thousands').value*1000;
+    this.fivehundreds = this.eodTally.get('FiveHundreds').value*500;
+    this.hundreds = this.eodTally.get('Hundreds').value*100;
+    this.fifty = this.eodTally.get('Fiftys').value*50;
+    this.tens = this.eodTally.get('Tens').value*10;
+    this.fives = this.eodTally.get('Fives').value*5;
+    this.ones = this.eodTally.get('Ones').value;
+
+    
+    this.total =this.thousands+ this.fivehundreds + this.hundreds + this.fifty + this.tens + this.fives + this.ones
+    console.log('sum',this.total);
+
+
   }
 
 }
